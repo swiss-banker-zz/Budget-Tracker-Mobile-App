c6fae693f773306ac38bf1651491b20eb1f25507
Kyle Lai <kl655@cornell.edu>
2020-08-24 16:37:56 -0400

    Added native base

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Text, Container } from 'native-base';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Container style={{ marginTop: '30%' }}>
+        <Text>Open up App.js to start working on your app!Hehe</Text>
+      </Container>
       <StatusBar style="auto" />
     </View>
   );
