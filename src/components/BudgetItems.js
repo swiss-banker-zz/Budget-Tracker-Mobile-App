ef0d976e4878914e25da82e3504bee4b652cbeb2
Kyle Lai <kl655@cornell.edu>
2020-08-24 16:46:22 -0400

    Added starter files

---
+++ src/components/BudgetItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text, Container } from 'native-base';
+
+export default function App() {
+  return (
+    <View>
+      <Container style={{ marginTop: '30%' }}>
+        <Text>BudgetItems</Text>
+      </Container>
+    </View>
+  );
+}
+
+
